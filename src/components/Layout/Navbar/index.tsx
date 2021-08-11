2862ad2f811fce0faaba9c08c10d8ace2caeac43
Jungmin Lee <mina.lee066@gmail.com>
2021-08-11 18:21:43 +0900

    Navbar is added

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import Link from "next/link";
+import classNames from "classnames";
+import ConunLogo from "@/assets/icons/conun-logo-head-bar.svg";
 
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
-  return <div className={styles.Navbar}>NAV BAR</div>;
+  const PAGES = [
+    {
+      id: "main",
+      name: "Main",
+      path: "/main",
+      show: true,
+    },
+    {
+      id: "smart-contract",
+      name: "Smart Contract",
+      path: "/smart",
+      show: true,
+    },
+    {
+      id: "docs",
+      name: "Docs",
+      path: "/docs",
+      show: true,
+    },
+  ];
+  return (
+    <div className={styles.Navbar}>
+      <Link href="/#home">
+        <a>
+          <ConunLogo className={classNames(styles.ConunLogo)} />
+        </a>
+      </Link>
+      {PAGES.map((nav) => {
+        return (
+          <a
+            key={nav.id}
+            href={nav.path}
+            className={classNames(styles.Nav)}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {nav.name}
+          </a>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Navbar;
