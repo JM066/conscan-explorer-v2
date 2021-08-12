b82416933425e45b1252eb4fbb50302f4b3708f0
Jungmin Lee <mina.lee066@gmail.com>
2021-08-12 18:14:48 +0900

    Added a list of social media icons and corresponding links

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,18 +1,61 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import { PAGES_FOOTER } from "../../../const";
-
+import { SECTIONS } from "../../../const";
+import World from "@/assets/icons/world.svg";
+import Logo from "@/assets/icons/conunLogoFooter.svg";
 import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
     <div className={styles.Footer}>
-      {PAGES_FOOTER.map((logo) => (
-        <Link key={logo.id} href={logo.path}>
-          <a>{logo.id}</a>
-        </Link>
-      ))}
+      <div className={styles.FooterItems}>
+        <div className={styles.WorldCell}>
+          <World className={styles.World} />
+        </div>
+
+        <div className={styles.IconContainer}>
+          <div className={styles.LogoCell}>
+            <Logo className={styles.Logo} />
+            <p>Powered by Conun</p>
+          </div>
+          <div className={styles.Icons}>
+            {PAGES_FOOTER.map((logo) => (
+              <Link key={logo.id} href={logo.path}>
+                <a>
+                  <Image src={logo.icon} alt={logo.id} />
+                </a>
+              </Link>
+            ))}
+          </div>
+          <div className={styles.Version}>
+            Conun Blockchain Explorer V.2.0.0
+          </div>
+        </div>
+        <div className={styles.CompanyCell}>
+          <p>Company</p>
+          {SECTIONS.company.map((company) => {
+            return (
+              <Link key={company.id} href={company.path}>
+                <a className={styles.Company}>{company.name}</a>
+              </Link>
+            );
+          })}
+        </div>
+        <div className={styles.CommunityCell}>
+          <p>Community</p>
+          {SECTIONS.community.map((community) => {
+            return (
+              <Link key={community.id} href={community.path}>
+                <a className={styles.Community}>{community.name}</a>
+              </Link>
+            );
+          })}
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
--- src/const/index.tsx
+++ src/const/index.tsx
@@ -1,3 +1,10 @@
+import tele from "@/assets/icons/telegramIcon.png";
+import face from "@/assets/icons/facebookIcon.png";
+import twit from "@/assets/icons/twitterIcon.png";
+import kack from "@/assets/icons/kakaoIcon.png";
+import git from "@/assets/icons/githubIcon.png";
+import disc from "@/assets/icons/discordIcon.png";
+
 export const PAGES_HEADER = [
   {
     id: "main",
@@ -19,26 +26,64 @@ export const PAGES_HEADER = [
 export const PAGES_FOOTER = [
   {
     id: "telegram",
+    icon: tele,
     path: "https://t.me/conun_global",
   },
   {
     id: "facebook",
+    icon: face,
     path: "https://www.facebook.com/conunglobal",
   },
   {
     id: "twitter",
+    icon: twit,
     path: "https://twitter.com/conunglobal",
   },
   {
     id: "kakao",
+    icon: kack,
     path: "https://open.kakao.com/o/gCsycmhb",
   },
   {
     id: "github",
+    icon: git,
     path: "https://github.com/CONUN-Global/CONUN",
   },
   {
     id: "discord",
+    icon: disc,
     path: "https://discord.gg/VvXvQfa3Za",
   },
 ];
+
+export const SECTIONS = {
+  company: [
+    {
+      id: "about-us",
+      name: "About Us",
+      path: "https://discord.gg/VvXvQfa3Za",
+    },
+    {
+      id: "contact-us",
+      name: "Contact Us",
+      path: "https://discord.gg/VvXvQfa3Za",
+    },
+    {
+      id: "terms-of-service",
+      name: "Terms of Service",
+      path: "https://conun.io/policy/termsandcondition",
+    },
+  ],
+  community: [
+    {
+      id: "main-site",
+      name: "Main Site",
+      path: "https://conun.io/",
+    },
+    {
+      id: "discord",
+      name: "Discord",
+      path: "https://discord.gg/VvXvQfa3Za/",
+    },
+  ],
+};
