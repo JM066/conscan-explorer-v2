041f1b14d9a629d1f02cab824b4685e4b09d69dd
Jungmin Lee <mina.lee066@gmail.com>
2021-08-25 18:06:03 +0900

    Button components types and props added

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,49 @@
+import classNames from "classnames";
+
+import Spinner from "../Loading";
+
+import styles from "./Button.module.scss";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className: any;
+  noStyle?: boolean;
+  round?: boolean;
+  loading?: boolean;
+  variant?: "primary" | "secondary";
 }
 
-function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+function Button({
+  children,
+  className,
+  noStyle,
+  round,
+  loading = false,
+  variant = "primary",
+  ...props
+}: ButtonProps) {
+  if (noStyle) {
+    return (
+      <button
+        className={classNames(styles.NoStyleButton, className)}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+  return (
+    <button
+      className={classNames(
+        styles.Button,
+        styles[variant],
+        { [styles.round]: round },
+        className
+      )}
+      {...props}
+    >
+      {loading ? <Spinner /> : children}
+    </button>
+  );
 }
 
 export default Button;
