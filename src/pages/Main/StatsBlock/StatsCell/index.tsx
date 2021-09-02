26733ceb8bf8833d7c9e2c9f4cf39ab4f01c590e
bruceji7 <revolverc24@gmail.com>
2021-09-02 16:58:20 +0900

    Add: counter stats table

--- src/pages/Main/StatsBlock/StatsCell/index.tsx
+++ src/pages/Main/StatsBlock/StatsCell/index.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import Panel from "@/components/Panel";
 import TwinPanel from "@/components/TwinPanel";
 
+import NetworkStats from "./NetworkStats";
+
 import styles from "./StatsCell.module.scss";
 
 function StatsCell() {
   return (
     <TwinPanel isLeft className={styles.Container}>
-      <Panel className={styles.Top}>Top</Panel>
+      <NetworkStats />
       <Panel className={styles.Bottom}>Bottom</Panel>
     </TwinPanel>
   );
