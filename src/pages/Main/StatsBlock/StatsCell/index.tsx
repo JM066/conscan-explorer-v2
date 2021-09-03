5e9ed5e5b29770dcda90351196ccd9209fc3daf0
bruceji7 <revolverc24@gmail.com>
2021-09-03 11:15:30 +0900

    Add: App services panel

--- src/pages/Main/StatsBlock/StatsCell/index.tsx
+++ src/pages/Main/StatsBlock/StatsCell/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Panel from "@/components/Panel";
 import TwinPanel from "@/components/TwinPanel";
 
 import NetworkStats from "./NetworkStats";
 
 import styles from "./StatsCell.module.scss";
+import Services from "./Services";
 
 function StatsCell() {
   return (
     <TwinPanel isLeft className={styles.Container}>
       <NetworkStats />
-      <Panel className={styles.Bottom}>Bottom</Panel>
+      <Services />
     </TwinPanel>
   );
 }
