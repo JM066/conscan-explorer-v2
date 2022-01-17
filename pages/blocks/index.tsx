4add5f2a3f19933d0017ae8006dd73887f912666
Jungmin Lee <mina.lee066@gmail.com>
2022-01-17 18:32:22 +0900

    console log deleted

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -6,10 +6,6 @@ export async function getStaticProps() {
   const latestBlocks = await getLatestBlocks();
   const channelHash = await getHash();
 
-  // const latestBlocks = await fetch(
-  //   `http://192.168.100.208:8080/api/blockActivity/${channelHash}?blocknum=${20}`
-  // );
-
   return {
     props: {
       channelHash: channelHash,
