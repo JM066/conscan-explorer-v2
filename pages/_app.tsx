4d96f21a067fe566a19e1326c9c40df40642bba4
Jungmin Lee <mina.lee066@gmail.com>
2022-01-18 18:23:06 +0900

    Path corrected using axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,16 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-import useWebSocket from "../src/hooks/useWebSocket";
-
 import Layout from "../src/components/Layout";
 import { queryClient } from "../src/react-query/config";
 
 import "./styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const webSocket = useWebSocket();
   return (
     <div>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
-        {webSocket}
         <Layout>
           <Component {...pageProps} />
         </Layout>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import useWebSocket from "@/hooks/useWebSocket";
 // import StatsBlock from "./StatsBlock";
 import BlocksActivitySection from "./BlocksActivitySection";
 // import TxnActivitySection from "./TxnActivitySection";
@@ -7,6 +7,7 @@ import BlocksActivitySection from "./BlocksActivitySection";
 import styles from "./Main.module.scss";
 
 function Main() {
+  useWebSocket();
   return (
     <div className={styles.Container}>
       {/* <StatsBlock /> */}
